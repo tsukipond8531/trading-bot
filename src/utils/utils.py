4cc4043432a36e53dfabf85847b02daa33014a72
tsukipond8531 <reader2027@gmail.com>
2024-05-01 18:47:39 +0200

    significant rounding

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,11 +4,29 @@ import os
 from src.config import TRADING_DATA_DIR
 
 
-def round(num, places):
-    working = str(num - int(num))
-    for i, e in enumerate(working[2:]):
-        if e != '0':
-            return int(num) + float(working[:i + 2 + places])
+def significant_round(num, places):
+    """
+    Custom rounding to a specific number of significant figures
+    starting after the first non-zero digit.
+
+    Parameters:
+    num (float): The number to be rounded.
+    places (int): The number of significant digits to retain after the first non-zero digit.
+
+    Returns:
+    float: The rounded number with the specified significant figures.
+    """
+    # Get the fractional part
+    fraction_part = str(num - int(num))
+
+    # Iterate through the fractional part to find the first non-zero digit
+    for i, digit in enumerate(fraction_part[2:], start=2):
+        if digit != '0':
+            # Concatenate integer part and significant fraction part
+            return int(num) + float(fraction_part[:i + places])
+
+    # If no non-zero digit is found, return the original number
+    return num
 
 
 def save_json_to_file(json_data, file_name):
